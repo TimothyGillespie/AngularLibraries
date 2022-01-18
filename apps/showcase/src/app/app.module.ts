1ed384dd0a0fe1d0a4477e15a3a9fb45d5b106d9
TimothyGillespie <timothy@gillespie.eu>
2022-01-18 06:43:15 +0100

    Begin start of youtube player showcase

--- apps/showcase/src/app/app.module.ts
+++ apps/showcase/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { AppComponent } from './app.component';
 import { CommonModule } from '@angular/common';
 import {YouTubePlayerModule} from "@tgillespie/youtube-player";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
 
 @NgModule({
   declarations: [AppComponent],
@@ -12,6 +14,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     CommonModule,
     YouTubePlayerModule.forRoot(),
     BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
