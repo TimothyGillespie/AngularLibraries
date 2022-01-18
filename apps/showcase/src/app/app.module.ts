b8fe350479d81b746ddb43b2c51d4dd7a3df2ff6
TimothyGillespie <timothy@gillespie.eu>
2022-01-18 06:07:57 +0100

    Adapt showcase to new import style

--- apps/showcase/src/app/app.module.ts
+++ apps/showcase/src/app/app.module.ts
@@ -1,19 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import {YouTubePlayerStandardModule} from "@tgillespie/youtube-player";
+import {YouTubePlayerModule} from "@tgillespie/youtube-player";
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     CommonModule,
-    FormsModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
-    YouTubePlayerStandardModule,
+    YouTubePlayerModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent],
